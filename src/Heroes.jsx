e00be80bc15c862f6a73e04a56414899b80ec2da
Elia <EliaRenov04@gmail.com>
2023-04-29 11:27:23 +0300

    Add heroes database

--- src/Heroes.jsx
+++ src/Heroes.jsx
@@ -40,7 +40,147 @@ const Heroes = {
     ana: {
         logo: Ana,
         color: '#8898b8'
-    }
+    },
+    ashe: {
+        logo: Ashe,
+        color: '#85878a'
+    },
+    baptiste: {
+        logo: Baptiste,
+        color: '#7eb9cf'
+    },
+    bastion: {
+        logo: Bastion,
+        color: '#909d91'
+    },
+    brigitte: {
+        logo: Brigitte,
+        color: '#9b8586'
+    },
+    cassidy: {
+        logo: Cassidy,
+        color: '#b88184'
+    },
+    doomfist: {
+        logo: Doomfist,
+        color: '#948081'
+    },
+    dva: {
+        logo: Dva,
+        color: '#f7a0cb'
+    },
+    echo: {
+        logo: Echo,
+        color: '#a7d2fb'
+    },
+    genji: {
+        logo: Genji,
+        color: '#a1f781'
+    },
+    hanzo: {
+        logo: Hanzo,
+        color: '#c0bb99'
+    },
+    junkerqueen: {
+        logo: Junkerqueen,
+        color: '#8db5d8'
+    },
+    junkrat: {
+        logo: Junkrat,
+        color: '#f3c283'
+    },
+    kiriko: {
+        logo: Kiriko,
+        color: '#d68a93'
+    },
+    lifeweaver: {
+        logo: Lifeweaver,
+        color: '#dacbdb'
+    },
+    lucio: {
+        logo: Lucio,
+        color: '#95cf83'
+    },
+    mei: {
+        logo: Mei,
+        color: '#88b3f0'
+    },
+    mercy: {
+        logo: Mercy,
+        color: '#f5f0c1'
+    },
+    moira: {
+        logo: Moira,
+        color: '#a28ce8'
+    },
+    orisa: {
+        logo: Orisa,
+        color: '#7c9b82'
+    },
+    pharah: {
+        logo: Pharah,
+        color: '#7b91cb'
+    },
+    ramatrra: {
+        logo: Ramatrra,
+        color: '#a090d3'
+    },
+    reaper: {
+        logo: Reaper,
+        color: '#907d83'
+    },
+    reinhardt: {
+        logo: Reinhardt,
+        color: '#9ea9ad'
+    },
+    roadhog: {
+        logo: Roadhog,
+        color: '#be9a83'
+    },
+    sigma: {
+        logo: Sigma,
+        color: '#9faaad'
+    },
+    sojourn: {
+        logo: Sojourn,
+        color: '#da8685'
+    },
+    soldier76: {
+        logo: Ana,
+        color: '#878ea1'
+    },
+    symmetra: {
+        logo: Symmetra,
+        color: '#9dc4d5'
+    },
+    torbjorn: {
+        logo: Torbjorn,
+        color: '#c68c8b'
+    },
+    tracer: {
+        logo: Tracer,
+        color: '#e0a181'
+    },
+    widowmaker: {
+        logo: Widowmaker,
+        color: '#907d83'
+    },
+    winston: {
+        logo: Winston,
+        color: '#aaaec1'
+    },
+    wreckingball: {
+        logo: Wreckingball,
+        color: '#7c9b82'
+    },
+    zarya: {
+        logo: Zarya,
+        color: '#f293bd'
+    },
+    zenyatta: {
+        logo: Zenyatta,
+        color: '#f7ed93'
+    },
 }
 
 export default Heroes
